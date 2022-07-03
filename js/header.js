19d819a851d1af8c0d27d18f57753f381c64a750
Nursultan <nurs12book@gmail.com>
2022-07-03 18:38:04 +0600

    first commit

---
+++ js/header.js
@@ -0,0 +1,8 @@
+function headerModal(text){
+    const hamMenu = document.querySelector(".header__hambur-menu")
+    const moda = document.querySelector(".header-modal")
+
+    moda.style.display = text
+
+    document.body.style.overflow = "none"
+}
